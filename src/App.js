155d4d30a47d1c1a894b08849e9b03d77cada89a
naimdasb <naimdasb@gmail.com>
2020-12-22 21:49:35 -0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [disableAdd, setDisableAdd] = useState(false);
+  const [disableSub, setDisableSub] = useState(false);
+
+  const handleClick = (event) => {
+    if (event.target.name === "add") {
+      if (counter < 10) {
+        setCounter(counter + 1);
+      } else {
+        setDisableAdd(true);
+        setDisableSub(false);
+      }
+    } else {
+      if (counter > 0) {
+        setCounter(counter - 1);
+      } else {
+        setDisableAdd(false);
+        setDisableSub(true);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Counter</h1>
+      <h5>{counter}</h5>
+      <button name="add" onClick={handleClick} disabled={disableAdd}>
+        Add
+      </button>
+      <button name="sub" onClick={handleClick} disabled={disableSub}>
+        Sub
+      </button>
     </div>
   );
 }
-
-export default App;
